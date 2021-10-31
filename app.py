c7487127544b9110c7b45bc050422321338dbe25
rachelp4515 <rachel.porhammer@students.dominican.edu>
2021-10-30 23:18:58 -0400

    users can make playlists

--- app.py
+++ app.py
@@ -4,19 +4,50 @@ client = MongoClient()
 db = client.Playlister
 playlists = db.playlists
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# playlists = [
-#     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-# ]
+def video_url_creator(id_lst):
+    videos = []
+    for vid_id in id_lst:
+        # We know that embedded YouTube videos always have this format
+        video = 'https://youtube.com/embed/' + vid_id
+        videos.append(video)
+    return videos
+
 
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists.find())
+
+@app.route('/playlists/new')
+def playlists_new():
+    """Create a new playlist."""
+    return render_template('playlists_new.html')
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """Submit a new playlist."""
+    video_ids = request.form.get('video_ids').split()
+    videos = video_url_creator(video_ids)
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
